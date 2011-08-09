5e28bda5091b8def41bb220fa274e4e293382f9b
Gergely Imreh <imrehg@gmail.com>
2011-08-09 20:08:31 +0200

    nginx build configuration tool

---
+++ software/nginx.doconf.sh
@@ -0,0 +1,16 @@
+./configure \
+	--prefix=/usr \
+	--pid-path=/var/run/nginx.pid \
+	--sbin-path=/usr/sbin/nginx \
+	--conf-path=/etc/nginx/nginx.conf \
+	--lock-path=/var/lock/nginx.lock \
+	--error-log-path=/var/log/nginx/error.log \
+	--user=web \
+	--group=www-data \
+	--with-http_ssl_module \
+	--with-http_gzip_static_module \
+	--with-http_stub_status_module \
+	--with-http_secure_link_module \
+	--http-fastcgi-temp-path=/var/tmp/nginx/fcgi/ \
+	--http-scgi-temp-path=/var/tmp/nginx/scgi/ \
+	--http-uwsgi-temp-path=/var/tmp/nginx/uwsgi/ \
